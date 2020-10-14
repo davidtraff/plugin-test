ab3c6817debbbe3d453746369caadba89736be1c
David <traff.david@gmail.com>
2020-10-14 12:37:24 +0200

    aaaaaa

--- public/plugin.js
+++ public/plugin.js
@@ -1,11 +1,16 @@
 console.log('Plugin loading...');
 
 const plugin = (props) => {
+    /* Ska ju egentligen bli transpilad från JSX så det hade ju sett ut mer typ
+    return (<h1>Root-appen säger {props.text}</h1>);
+    */
     return React.createElement('h1', {
         children: `Root-appen säger ${props.text}`,
     });
 };
 
+// pusha på den på nått globalt state, eller ännu bättre om man skickar med pluginen i ett argument i eventet.
 (window.plugins || (window.plugins = [])).push({ name: 'skit', plugin });
 
+// bam
 window.dispatchEvent(new Event('plugin-loaded'));
--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,17 @@ function App() {
 
     const pluginAdded = () => {
         console.log('Lägger till plugins');
-        setPlugins((window as any).plugins);
+        setPlugins([...(window as any).plugins]); // Kopiera för att inte mutera staten.
     };
 
     React.useEffect(() => {
+        // Lyssna efter eventet från plugin.js.
         window.addEventListener('plugin-loaded', pluginAdded);
 
         return () => window.removeEventListener('plugin-loaded', pluginAdded);
     }, []);
 
+    // Sen skapar vi bara upp varje element här med createElement. Nu kommer den ligga under samma root å allt så man kan smacka på med context, props, etc hur man vill.
     return (
         <div className="App">
             <p>Main-appen</p>
