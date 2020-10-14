863f9f381da2a0776981e25569acdcdb755d88c7
David <traff.david@gmail.com>
2020-10-14 12:39:50 +0200

    aaa

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// Viktigt här är ju att exponera React-libbet på något vis till alla plugins. Detta funkar ju, men inte så värst optimalt.
 (window as any).React = React;
 
 ReactDOM.render(
